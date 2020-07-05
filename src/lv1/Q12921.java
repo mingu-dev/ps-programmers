d7e7bcb24126f3e48382a86778caef558052abcd
mangojuicedeli <mgkang0317@gmail.com>
2020-07-05 20:11:28 +0900

    Q12921

---
+++ src/lv1/Q12921.java
@@ -0,0 +1,30 @@
+package lv1;
+
+public class Q12921 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+	public int solution(int n) {
+		int answer = 0;
+				
+		for (int i = 2; i <= n; i++) {
+			boolean isPrime = true;
+			if (i == 2 || i == 3) {
+				answer++;
+				continue;
+			}
+			
+			for (int j = 2; j*j <= i; j++) {
+				if (i % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime == true)
+				answer++;
+		}
+		return answer;
+	}
+}
