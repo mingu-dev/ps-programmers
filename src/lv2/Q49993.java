4574826aa68482eb090c9d329792277d0b053489
mangojuicedeli <mgkang0317@gmail.com>
2020-11-09 19:14:08 +0900

    Q49993

---
+++ src/lv2/Q49993.java
@@ -0,0 +1,33 @@
+package lv2;
+
+public class Q49993 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int solution(String skill, String[] skill_trees) {
+		int answer = 0;
+		int index = 0;
+		
+		while (true) {
+			String skill_tree = skill_trees[index];
+			String skill_clone = new String(skill_tree);
+			int size = skill_tree.length();
+			for (int i = 0; i < size; i++) {
+				String oneSkill = String.valueOf(skill_clone.charAt(i));
+				if (!skill.contains(oneSkill)) {
+					skill_tree = skill_tree.replace(oneSkill, "");
+				}
+			}
+			if (skill.indexOf(skill_tree) == 0) {
+				answer++;
+			}
+			index++;
+			if (index == skill_trees.length)
+				break;
+		}
+		return answer;
+	}
+}
